ba9875008bddd33a65e47a4f837d7143c1035fef
Jae <jaeladu1@gmail.com>
2024-01-22 12:30:52 -0300

    Added test endpoint

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { reqVerbsHandler } from "src/lib/middlewares";
 
 export default reqVerbsHandler({
-   get: {
+   post: {
       callback: (req: NextApiRequest, res: NextApiResponse) =>
          res.send("Hola test"),
    },
