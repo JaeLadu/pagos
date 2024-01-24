3d8071087358f50e48161ef11ade7cd72a8578b5
Jae <jaeladu1@gmail.com>
2024-01-24 11:20:06 -0300

    Modifiqué test Lo llevo a lo más básico posible, que responda a todos los métodos con 200 y un texto para probar el error de 405 con post en next

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
-export default reqVerbsHandler({
-   post: {
-      callback: (req: NextApiRequest, res: NextApiResponse) =>
-         res.send("Hola test"),
-   },
-});
+export default function (res: NextApiResponse) {
+   res.status(200).send("test");
+}
