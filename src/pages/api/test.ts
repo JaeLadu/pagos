9c7a2d7b8a551034ea6fb829f19d2dbab391a70b
Jae <jaeladu1@gmail.com>
2024-01-24 11:23:40 -0300

    modifico test
    Hago que devuelva una función

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,5 +2,5 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
 export default function (res: NextApiResponse) {
-   res.status(200).send("test");
+   return () => res.status(200).send("test");
 }
