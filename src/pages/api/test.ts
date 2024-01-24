67828d400eff47145e2f022fc26e9dd35f21f892
Jae <jaeladu1@gmail.com>
2024-01-24 11:32:58 -0300

    Agrego nombre a función de test

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
-export default function (res: NextApiResponse) {
-   return () => res.status(200).send("test");
+export default function handler(res: NextApiResponse) {
+   res.status(200).send("test");
 }
