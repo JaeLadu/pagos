0868b02abbc17438385748a63945a31ac8f23111
Jae <jaeladu1@gmail.com>
2024-01-25 14:14:05 -0300

    Modified test
    Changed the file to only have a function that returns 'test' and status 200

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,23 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
    res.status(200).send("test");
 }
-async function asincrona(req: NextApiRequest, res: NextApiResponse) {
-   try {
-      await fetch("https://serverles-api.vercel.app/api/search?busqueda=123");
-
-      return handler(req, res);
-   } catch (error) {
-      res.status(error.status || 500).end(error.message);
-   }
-}
-
-export default (req, res) =>
-   reqVerbsHandler(req, res, {
-      post: {
-         callback: asincrona,
-         middleWares: [checkToken],
-      },
-   });
