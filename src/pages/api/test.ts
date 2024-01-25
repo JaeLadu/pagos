c6c4830002a9fc861a0b2bcd91cd1152e027c8b0
Jae <jaeladu1@gmail.com>
2024-01-25 14:22:17 -0300

    Modified test 2
    Added req manipulation and changed res

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -2,5 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-   res.status(200).send("test");
+   const { method, query, body } = req;
+   res.status(200).send(
+      `test. method: ${method}, query: ${JSON.stringify(
+         query
+      )}, body: ${JSON.stringify(body)}`
+   );
 }
