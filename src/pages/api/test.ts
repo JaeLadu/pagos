f27d453c099194e56dcd38b0ad9ea8626e89cd04
Jae <jaeladu1@gmail.com>
2024-01-26 16:14:25 -0300

    Modified test 5
    Added middlewares

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,11 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
-// const mockObj = {
-//    post: {
-//       callback: mockFun,
-//    },
-// };
+const middlewares = [
+   (req, res) => {
+      req.body = { ...req.body, middleOne: true };
+      return { req, res };
+   },
+   (req, res) => {
+      req.body = { ...req.body, middleTwo: true };
+      return { req, res };
+   },
+   (req, res) => {
+      req.body = { ...req.body, middleThree: true };
+      return { req, res };
+   },
+];
+const mockObj = {
+   post: {
+      callback: returnFun,
+      middleWares: middlewares,
+   },
+};
 
 function returnFun(req, res) {
    const { query, body } = req;
@@ -21,24 +36,33 @@ function returnFun(req, res) {
 function mockFun(req, res) {
    res.status(200).end("Mock");
 }
-// function filter(verbsObj, req, res) {
-//    const requestMethod = req.method!.toLowerCase();
-//    const isMethodAllowed = mockObj[requestMethod];
+function filter(req, res, verbsObj) {
+   const requestMethod = req.method!.toLowerCase();
+   const isMethodAllowed = verbsObj[requestMethod];
 
-//    if (!isMethodAllowed) {
-//       res.status(405).end("Method not allowed");
-//       return;
-//    }
+   if (!isMethodAllowed) {
+      res.status(405).end("Method not allowed");
+      return;
+   }
 
-//    const callback: Function = verbsObj[requestMethod].callback;
+   const callback: Function = verbsObj[requestMethod].callback;
+   const middleWares: Function[] | Promise<Function>[] =
+      verbsObj[requestMethod].middleWares;
 
-//    callback(req, res);
-// }
+   if (middleWares) {
+      middleWares.forEach(async (m) => {
+         try {
+            const response = await m(req, res);
+            req = response.req;
+            res = response.res;
+         } catch (error) {
+            console.log(error.message);
+            return;
+         }
+      });
+   }
 
-export default (req, res) =>
-   reqVerbsHandler(req, res, {
-      post: {
-         callback: returnFun,
-         middleWares: [checkToken],
-      },
-   });
+   callback(req, res);
+}
+
+export default (req, res) => filter(req, res, mockObj);
