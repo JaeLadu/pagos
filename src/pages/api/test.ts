70239e49dc5ff7bd8a4ff44ed68d2e2342756afd
Jae <jaeladu1@gmail.com>
2024-01-26 13:06:21 -0300

    Modified test 3
    Added check for method and 405 return if its not allowed

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,11 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkToken, reqVerbsHandler } from "src/lib/middlewares";
 
+const mockObj = {
+   post: {
+      callback: filter,
+   },
+};
+
+function filter(req, res) {
+   const { query, body } = req;
+
+   const requestMethod = req.method!.toLowerCase();
+   const isMethodAllowed = mockObj[requestMethod];
+
+   if (!isMethodAllowed) {
+      res.status(405).end("Method not allowed");
+      return;
+   }
+   return res
+      .status(200)
+      .send(
+         `test. method: ${req.method}, query: ${JSON.stringify(
+            query
+         )}, body: ${JSON.stringify(body)}`
+      );
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-   const { method, query, body } = req;
-   res.status(200).send(
-      `test. method: ${method}, query: ${JSON.stringify(
-         query
-      )}, body: ${JSON.stringify(body)}`
-   );
+   return filter(req, res);
 }
