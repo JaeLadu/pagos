1f1738f5af1ef5b324fe33105f39cb8069b6b1ae
Jae <jaeladu1@gmail.com>
2024-01-31 15:58:04 -0300

    Minor mod
    Added body parsing in middlewares for server to work

--- src/lib/middlewares.ts
+++ src/lib/middlewares.ts
@@ -57,7 +57,7 @@ export async function reqVerbsHandler(
 
 export async function checkToken(req: NextApiRequest, res: NextApiResponse) {
    const token = req.headers.authorization?.split(" ")[1];
-   // req.body = JSON.parse(req.body);
+   req.body = JSON.parse(req.body);
    const { email } = req.body;
    try {
       if (!token) {
