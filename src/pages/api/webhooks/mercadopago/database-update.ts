56f0879669d1c29d1f01d3870675f3432372da8f
Jae <jaeladu1@gmail.com>
2024-02-01 16:39:11 -0300

    Changed database endpoint
    Modified function to work with body from the request instead of query

--- src/pages/api/webhooks/mercadopago/database-update.ts
+++ src/pages/api/webhooks/mercadopago/database-update.ts
@@ -3,17 +3,14 @@ import { reqVerbsHandler } from "src/lib/middlewares";
 import { Order } from "src/lib/models/order";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-   const { id, topic } = req.query;
-   if (topic == "merchant_order") {
-      const response = await fetch(
-         `https://api.mercadolibre.com/merchant_orders/${id}`,
-         {
-            headers: {
-               "Content-Type": "application/json",
-               Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
-            },
-         }
-      );
+   const { resource } = req.body;
+   if (resource) {
+      const response = await fetch(`${resource}`, {
+         headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
+         },
+      });
       const data = await response.json();
       const { external_reference, order_status } = data;
       const order = new Order(external_reference);
