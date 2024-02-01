53ba39c8d41a4326a951aa45ea8444ed49655a31
Jae <jaeladu1@gmail.com>
2024-02-01 14:37:36 -0300

    Moved enpoint logic to diferent file
    Moved all the logic for updating the preference in de DB out of the endpoint to prevent 504 timeout

--- src/lib/mercadopago.ts
+++ src/lib/mercadopago.ts
@@ -1,5 +1,7 @@
 import MercadoPagoConfig, { Preference } from "mercadopago";
 import { Product } from "./models/product";
+import { Order } from "./models/order";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const client = new MercadoPagoConfig({
    accessToken: process.env.MP_TEST_ACCESS_TOKEN!,
@@ -21,4 +23,26 @@ export async function createMPPreference(data: {
    });
 
    return response.init_point;
+}
+
+export async function syncPreference(
+   req: NextApiRequest,
+   res: NextApiResponse
+) {
+   const { id, topic } = req.query;
+   if (topic == "merchant_order") {
+      const response = await fetch(
+         `https://api.mercadolibre.com/merchant_orders/${id}`,
+         {
+            headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
+            },
+         }
+      );
+      const data = await response.json();
+      const { external_reference, order_status } = data;
+      const order = new Order(external_reference);
+      await order.syncDataBase({ status: order_status });
+   }
 }
--- src/pages/api/webhooks/mercadopago.ts
+++ src/pages/api/webhooks/mercadopago.ts
@@ -1,25 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { syncPreference } from "src/lib/mercadopago";
 import { reqVerbsHandler } from "src/lib/middlewares";
-import { Order } from "src/lib/models/order";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-   const { id, topic } = req.query;
-   if (topic == "merchant_order") {
-      const response = await fetch(
-         `https://api.mercadolibre.com/merchant_orders/${id}`,
-         {
-            headers: {
-               "Content-Type": "application/json",
-               Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
-            },
-         }
-      );
-      const data = await response.json();
-      const { external_reference, order_status } = data;
-      const order = new Order(external_reference);
-      await order.syncDataBase({ status: order_status });
-      res.status(200).end();
-   }
+function handler(req: NextApiRequest, res: NextApiResponse) {
+   syncPreference(req, res);
+   res.status(200).end();
 }
 export default (req, res) =>
    reqVerbsHandler(req, res, {
