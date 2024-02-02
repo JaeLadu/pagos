8584bcbd607cb0dcc5086063c02383315bd7860a
Jae <jaeladu1@gmail.com>
2024-02-02 11:43:01 -0300

    Added error catching to database-update

--- src/pages/api/webhooks/mercadopago/database-update.ts
+++ src/pages/api/webhooks/mercadopago/database-update.ts
@@ -4,21 +4,27 @@ import { Order } from "src/lib/models/order";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
    const { resource } = JSON.parse(req.body);
-   if (resource) {
-      const response = await fetch(`${resource}`, {
-         headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
-         },
-      });
-      const data = await response.json();
-      const { external_reference, order_status } = data;
-      const order = new Order(external_reference);
-      await order.syncDataBase({ status: order_status });
-   } else {
-      throw new Error("Falló database-update");
+   try {
+      if (resource) {
+         const response = await fetch(`${resource}`, {
+            headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
+            },
+         });
+         const data = await response.json();
+         const { external_reference, order_status } = data;
+         const order = new Order(external_reference);
+         await order.syncDataBase({ status: order_status });
+      } else {
+         throw new Error("Falló database-update");
+      }
+      return res.status(200).end("Ok");
+   } catch (error) {
+      console.log(error.message);
+
+      throw new Error(error.message);
    }
-   return res.status(200).end("Ok");
 }
 
 export default (req: NextApiRequest, res: NextApiResponse) =>
