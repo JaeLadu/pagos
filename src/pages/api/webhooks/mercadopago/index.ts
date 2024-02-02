905729dfa04015268bae12f8fdfd03f702c68ef5
Jae <jaeladu1@gmail.com>
2024-02-02 12:00:58 -0300

    Added checking of body in MP endpoint

--- src/pages/api/webhooks/mercadopago/index.ts
+++ src/pages/api/webhooks/mercadopago/index.ts
@@ -2,13 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { reqVerbsHandler } from "src/lib/middlewares";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-   await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/webhooks/mercadopago/database-update`,
-      {
-         method: "POST",
-         body: JSON.stringify(req.body),
-      }
-   );
+   const { body } = req;
+   const { resource } = body;
+   if (resource) {
+      await fetch(
+         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/webhooks/mercadopago/database-update`,
+         {
+            method: "POST",
+            body: JSON.stringify(body),
+         }
+      );
+   }
    res.status(200).end();
 }
 export default (req, res) =>
