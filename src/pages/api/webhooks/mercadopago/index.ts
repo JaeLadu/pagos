2333a41bd7bf76b68fbe97d5d9ef3f3ebcf53781
Jae <jaeladu1@gmail.com>
2024-02-02 11:37:44 -0300

    Added async await to mp endpoint

--- src/pages/api/webhooks/mercadopago/index.ts
+++ src/pages/api/webhooks/mercadopago/index.ts
@@ -1,19 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { reqVerbsHandler } from "src/lib/middlewares";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-   try {
-      const response = fetch(
-         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/webhooks/mercadopago/database-update`,
-         {
-            method: "POST",
-            body: JSON.stringify(req.body),
-         }
-      );
-      console.log(response);
-   } catch (error) {
-      throw Error(`Falló index de mercado pago con el error: ${error.message}`);
-   }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+   await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/webhooks/mercadopago/database-update`,
+      {
+         method: "POST",
+         body: JSON.stringify(req.body),
+      }
+   );
    res.status(200).end();
 }
 export default (req, res) =>
